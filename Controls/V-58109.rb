b0ce1e6041e59861d6cb5415dea2e583d6550fb1
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-07-26 22:55:26 -0400

    Update V-58109.rb

--- Controls/V-58109.rb
+++ Controls/V-58109.rb
@@ -35,5 +35,13 @@ connection attempts occur.
   tag "fix_id": nil
   tag "cci": ["CCI-000172"]
   tag "nist": ["AU-12 c", "Rev_4"]
+  
+  describe file(input('couchdb_conf_local')) do
+    it { should exist }
+  end
+
+  describe ini(input('couchdb_conf_local')) do
+    its('log.level') { should match 'info' }
+  end
 end
 
