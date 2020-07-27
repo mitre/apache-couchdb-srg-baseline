e2ebd4b91fa7e86225abb8f9aab7bffeb33dcfd8
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-07-26 22:45:32 -0400

    Update V-32375.rb

--- Controls/V-32375.rb
+++ Controls/V-32375.rb
@@ -55,5 +55,13 @@ for audit events identified by type, location, or subject, this is a finding.
   tag "fix_id": nil
   tag "cci": ["CCI-000135"]
   tag "nist": ["AU-3 (1)", "Rev_4"]
+  
+  describe file(input('couchdb_conf_local')) do
+    it { should exist }
+  end
+
+  describe ini(input('couchdb_conf_local')) do
+    its('log.level') { should match 'info' }
+  end
 end
 
