5cbccd8bf2dee4617d78ce461270ed46c48bdf01
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-07-26 22:48:26 -0400

    Update V-58073.rb

--- Controls/V-58073.rb
+++ Controls/V-58073.rb
@@ -42,5 +42,13 @@ to add privileges/permissions occur.
   tag "fix_id": nil
   tag "cci": ["CCI-000172"]
   tag "nist": ["AU-12 c", "Rev_4"]
+  
+  describe file(input('couchdb_conf_local')) do
+    it { should exist }
+  end
+
+  describe ini(input('couchdb_conf_local')) do
+    its('log.level') { should match 'info' }
+  end
 end
 
