bff268ac016beac611dcb6841324b430201cc0a0
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 15:57:46 -0400

    Update V-58103.rb

--- controls/V-58103.rb
+++ controls/V-58103.rb
@@ -39,7 +39,7 @@ categories of information are deleted.
   tag "cci": ["CCI-000172"]
   tag "nist": ["AU-12 c", "Rev_4"]
   
-  describe ini(input('couchdb_conf_defaultt')) do
+  describe ini(input('couchdb_conf_default')) do
     its('log.level') { should eq 'info' }
   end
 end
