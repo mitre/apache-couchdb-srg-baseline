4c290149ebe753b66e393e6236a7821b37e0f4da
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 15:30:37 -0400

    Update V-32347.rb

--- controls/V-32347.rb
+++ controls/V-32347.rb
@@ -48,7 +48,7 @@ performed organization-defined actions.
   tag "cci": ["CCI-000166"]
   tag "nist": ["AU-10", "Rev_4"]
   
-  describe ini(input('couchdb_conf_defaultt')) do
+  describe ini(input('couchdb_conf_default')) do
     its('log.level') { should eq 'info' }
   end
 end
