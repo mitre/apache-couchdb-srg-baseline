30e147d7469c3234943ee2292244c81a6637b608
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 15:43:00 -0400

    Update V-32375.rb

--- controls/V-32375.rb
+++ controls/V-32375.rb
@@ -56,7 +56,7 @@ for audit events identified by type, location, or subject, this is a finding.
   tag "cci": ["CCI-000135"]
   tag "nist": ["AU-3 (1)", "Rev_4"]
   
-  describe ini(input('couchdb_conf_defaultt')) do
+  describe ini(input('couchdb_conf_default')) do
     its('log.level') { should eq 'info' }
   end
 end
