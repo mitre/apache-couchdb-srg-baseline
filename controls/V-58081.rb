44d281de064c805fbbf283ca2b6b24e618d829f6
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 14:39:15 -0400

    Update V-58081.rb

--- controls/V-58081.rb
+++ controls/V-58081.rb
@@ -43,10 +43,7 @@ delete privileges/permissions occur.
   tag "cci": ["CCI-000172"]
   tag "nist": ["AU-12 c", "Rev_4"]
   
-  describe file(input('couchdb_conf_default')) do
-    it { should exist }
-  end
-  describe ini(input('couchdb_conf_default')) do
+  describe ini(input('couchdb_conf_defaultt')) do
     its('log.level') { should eq 'info' }
   end
 end
