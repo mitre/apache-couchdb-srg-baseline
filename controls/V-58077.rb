50247943addbc8874b90ae18624eb7c93e6aafe3
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 15:54:02 -0400

    Update V-58077.rb

--- controls/V-58077.rb
+++ controls/V-58077.rb
@@ -43,9 +43,6 @@ modify privileges/permissions occur.
   tag "cci": ["CCI-000172"]
   tag "nist": ["AU-12 c", "Rev_4"]
   
-  describe file(input('couchdb_conf_default')) do
-    it { should exist }
-  end
   describe ini(input('couchdb_conf_default')) do
     its('log.level') { should eq 'info' }
   end
