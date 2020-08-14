6cbe140c3904ee6033b55209774e2e80f18284c9
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 14:31:59 -0400

    Update V-32362.rb

--- controls/V-32362.rb
+++ controls/V-32362.rb
@@ -56,10 +56,7 @@ finding.
   tag "cci": ["CCI-000169"]
   tag "nist": ["AU-12 a", "Rev_4"]
   
-  describe file(input('couchdb_conf_default')) do
-    it { should exist }
-  end
-  describe ini(input('couchdb_conf_default')) do
+  describe ini(input('couchdb_conf_defaultt')) do
     its('log.level') { should eq 'info' }
   end
 end
