51b35d47b591a042fd2c254b2df98b1801e140b6
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 14:34:01 -0400

    Update V-32371.rb

--- controls/V-32371.rb
+++ controls/V-32371.rb
@@ -50,10 +50,7 @@ information to establish the sources (origins) of the events.
   tag "cci": ["CCI-000133"]
   tag "nist": ["AU-3", "Rev_4"]
   
-  describe file(input('couchdb_conf_default')) do
-    it { should exist }
-  end
-  describe ini(input('couchdb_conf_default')) do
+  describe ini(input('couchdb_conf_defaultt')) do
     its('log.level') { should eq 'info' }
   end
 end
