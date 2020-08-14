8be48b19316cf6490261187b7e8029d8c42e4d31
courtney-hass <66285927+courtney-hass@users.noreply.github.com>
2020-08-14 15:59:07 -0400

    Update V-58119.rb

--- controls/V-58119.rb
+++ controls/V-58119.rb
@@ -48,7 +48,7 @@ objects occur.
   tag "cci": ["CCI-000172"]
   tag "nist": ["AU-12 c", "Rev_4"]
   
-  describe ini(input('couchdb_conf_defaultt')) do
+  describe ini(input('couchdb_conf_default')) do
     its('log.level') { should eq 'info' }
   end
 end
